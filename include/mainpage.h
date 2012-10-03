651e3f4472e2c59a686a12c9f83a50bdd045d2dc
Luke Teacy <wtlt@ecs.soton.ac.uk>
2012-10-03 09:48:38 +0100

    Added skeleton documentation to build.

---
+++ include/mainpage.h
@@ -0,0 +1,25 @@
+/**
+ * @file mainpage.h
+ * This header defines the main page for this documentation, and is for that
+ * purpose only.
+ * @author Dr W. T. Luke Teacy, University of Southampton
+ */
+namespace mcts {
+/**
+ * @mainpage
+ * \tableofcontents
+ * @section intro Overview
+ * This library is under development, but will eventually provide a
+ * C++ implementation for performing Monte Carlo Tree Search.
+ *
+ * Further information on Monte Carlo Tree Search techniques can be found
+ * <a href="http://www.mcts.ai">here</a>.
+ *
+ * The source code for this library can be dowloaded from
+ * <a href="https://github.com/lteacy/mcts-cpp">here</a>.
+ *
+ * If you have any comments or suggestions about this library or its
+ * documentation, please let me know. My contact details can be found
+ * here: http://users.ecs.soton.ac.uk/wtlt/
+ */
+}
